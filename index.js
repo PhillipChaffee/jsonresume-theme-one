279c2d54b1ea793295786862a5362bba33591ffd
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2022-07-07 16:56:00 -0700

    Process project highlights & turn all highlights into safe strings so I can render HTML.

--- index.js
+++ index.js
@@ -4,56 +4,60 @@ var Handlebars = require("handlebars");
 COURSES_COLUMNS = 3;
 
 PREPEND_SUMMARY_CATEGORIES = [
-  "work",
-  "volunteer",
-  "awards",
-  "publications"
+    "work",
+    "volunteer",
+    "awards",
+    "publications",
+    "projects"
 ];
 
 function validateArray(arr) {
-  return arr !== undefined && arr !== null && arr instanceof Array && arr.length > 0;
+    return arr !== undefined && arr !== null && arr instanceof Array && arr.length > 0;
 }
 
 function render(resume) {
-  // Split courses into 3 columns
-  if (validateArray(resume.education)) {
-    resume.education.forEach(function(block) {
-      if (validateArray(block.courses)) {
-        splitCourses = [];
-        columnIndex = 0;
-        for (var i = 0; i < COURSES_COLUMNS; i++) {
-          splitCourses.push([]);
-        }
-        block.courses.forEach(function(course) {
-          splitCourses[columnIndex].push(course);
-          columnIndex++;
-          if (columnIndex >= COURSES_COLUMNS) {
-            columnIndex = 0;
-          }
+    // Split courses into 3 columns
+    if (validateArray(resume.education)) {
+        resume.education.forEach(function (block) {
+            if (validateArray(block.courses)) {
+                splitCourses = [];
+                columnIndex = 0;
+                for (var i = 0; i < COURSES_COLUMNS; i++) {
+                    splitCourses.push([]);
+                }
+                block.courses.forEach(function (course) {
+                    splitCourses[columnIndex].push(course);
+                    columnIndex++;
+                    if (columnIndex >= COURSES_COLUMNS) {
+                        columnIndex = 0;
+                    }
+                });
+                block.courses = splitCourses;
+            }
         });
-        block.courses = splitCourses;
-      }
-    });
-  }
-
-  PREPEND_SUMMARY_CATEGORIES.forEach(function(category) {
-    if (resume[category] !== undefined) {
-      resume[category].forEach(function(block) {
-        if (block.highlights === undefined) {
-          block.highlights = [];
-        }
-      });
     }
-  });
 
-	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-	return Handlebars.compile(tpl)({
-		css: css,
-		resume: resume
-	});
+    PREPEND_SUMMARY_CATEGORIES.forEach(function (category) {
+        if (resume[category] !== undefined) {
+            resume[category].forEach(function (block) {
+                if (block.highlights === undefined) {
+                    block.highlights = [];
+                }
+                console.log(block.highlights);
+                block.highlights = block.highlights.map(hl => new Handlebars.SafeString(hl));
+                console.log(block.highlights);
+            });
+        }
+    });
+
+    var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+    var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+    return Handlebars.compile(tpl)({
+        css: css,
+        resume: resume
+    });
 }
 
 module.exports = {
-	render: render
+    render: render
 };
